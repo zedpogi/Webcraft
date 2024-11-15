96b4198f32598d3a2be02cf65f747ec7a7b80a0a
zedpogi <148332492+zedpogi@users.noreply.github.com>
2024-11-15 13:15:48 +0800

    Add files via upload

---
+++ projectpr/options/options.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Webcraft</title>
+    <link rel="stylesheet" href="options.css">
+</head>
+<body>
+    <header>
+        <h1 class="logo-text">Webcraft</h1>
+        <img src="webcraft_logo.png" alt="" class="logo">
+    </header>
+      <body>
+        
+        <div class="cards">
+            <div class="html-card" href="/projectpr/HTML-content/1-content.html">
+            </div>
+            
+            <div class="css-card"></div>
+            <div class="js-card"></div>
+        </div>
+
+        
+
+      </body>
+      </html>
+      <script src="switch.js"> </script>
+</body>
+</html>
