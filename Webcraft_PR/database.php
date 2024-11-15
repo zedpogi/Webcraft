24dba2e1d1ec4f2379d375f882284038be10f19a
zedpogi <148332492+zedpogi@users.noreply.github.com>
2024-11-15 10:44:22 +0800

    Add files via upload

---
+++ Webcraft_PR/database.php
@@ -0,0 +1,15 @@
+<?php
+    $db_server = "localhost";
+    $db_user = "root";
+    $pass = "";
+    $name = "userdb";
+    $conn = "";
+    try {
+    $conn = mysqli_connect(
+        $db_server, $db_user, $pass, $name
+    ); 
+    }
+    catch(mysqli_sql_exception) {
+        echo "connection failed";
+    }
+?>
