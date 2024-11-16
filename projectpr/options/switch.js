2380d8f23a17a340769b4bd9284fdd8b6ce2d117
zedpogi <148332492+zedpogi@users.noreply.github.com>
2024-11-16 18:22:21 +0800

    Delete projectpr directory

--- projectpr/options/switch.js
+++
@@ -1,7 +0,0 @@
-const htmlCard = document.querySelector('.html-card');
-
-function urlSwitch(url) {
-    window.location.href = "/projectpr/HTML-content/1-content.html";
-}
-
-htmlCard.addEventListener("click", urlSwitch);
