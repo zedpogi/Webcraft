fb9970911e95e999ea43de9c1e529fcc8ceaf17a
zedpogi <148332492+zedpogi@users.noreply.github.com>
2024-12-17 17:34:56 +0800

    Delete projectpr directory

--- projectpr/js-content/nextpage2/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('runButton').addEventListener('click', () => {
-    const code = document.getElementById('editor').value;
-    const outputFrame = document.getElementById('output');
-    const outputDoc = outputFrame.contentDocument || outputFrame.contentWindow.document;
-    outputDoc.open();
-    outputDoc.write(code);
-    outputDoc.close();
-});
